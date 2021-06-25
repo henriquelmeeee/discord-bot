5ca05638d03f573ad7423765ff524cde03cb01ab
henriquelmeeee <86432400+henriquelmeeee@users.noreply.github.com>
2021-06-25 15:26:31 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,23 +101,38 @@ async def github(ctx):
     )
     await ctx.send(embed = embed)
 
-
 saldo = 0
 
-@client.command()
-async def economia(ctx):
-    await ctx.send(f'Seu saldo é {saldo}.')
-    await ctx.send('A economia ainda não está disponível.')
+@client.command(aliases=['trabalho', 'job'])
+async def trabalhar(ctx):
+    global saldo
+    try:
+        if ctx.author.guild_permissions.administrator:
+            saldonovo = random.randint(15, 500)
+            saldo = saldo + saldonovo
+            await ctx.send(f'Você ganhou {saldonovo}! Agora, seu saldo é {saldo}.')
+        else:
+            await ctx.send('Por enquanto, apenas administradores podem usar esta função.')
+    except Exception as erro:
+        if ctx.author.guild_permissions.administrator:
+            await ctx.send(f'O erro "{erro}" ocorreu.')
+            await ctx.send('Cancelando ação... ')
+        else:
+            await ctx.send('Um erro ocorreu, mas você não tem permissão para vê-lo.')
+
+
+@client.command(aliases=['dinheiro'])
+async def saldo(ctx):
+    if ctx.author.guild_permissions.administrator:
+        await ctx.send(f'Seu saldo é {saldo}.')
+    else:
+        await ctx.send('Por enquanto, apenas administradores podem usar esta função.')
 
 
 @client.command(aliases=['a', 'suporte'])
 async def ajuda(ctx):
     embed = discord.Embed(
-
-        author = {
-                'name': 'Some name',
-                'url': 'https://discord.js.org'
-        },
+        
         title='Suporte',
         description='teste'
 
@@ -128,11 +143,14 @@ async def ajuda(ctx):
 
 @client.command(aliases=['say'])
 async def falar(ctx, *, mensagem=None):
-    if mensagem is None:
-        await ctx.send('Informe um valor válido.')
+    if ctx.author.guild_permissions.administrator:
+        if mensagem is None:
+            await ctx.send('Informe um valor válido.')
+        else:
+            await ctx.message.delete()
+            await ctx.send(mensagem)
     else:
-        await ctx.message.delete() #deletar mensagem do bot
-        await ctx.send(mensagem)
+        await ctx.send('Você não tem permissão para executar este comando.')
 
 
 client.run\
