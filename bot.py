932f34c8a6b8af122666944730046f634ee84a23
henriquelmeeee <86432400+henriquelmeeee@users.noreply.github.com>
2021-06-25 12:44:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,17 +18,24 @@ async def on_ready():
 
 @client.command(aliases=['coroa', 'cara', 'caraoucoroa', 'jogarmoeda'])
 async def moeda(ctx, caracoroa):
-    embed = discord.Embed(
+    if caracoroa == 'cara' or caracoroa == 'coroa':
+        embed = discord.Embed(
 
-        title='Cara ou Coroa',
-        description=f'A moeda escolhida foi: {random.choice(["CARA", "COROA"])}\n'
-                    f'e você escolheu: {caracoroa.upper()}',
-        colour=discord.Colour.blue()
+            title='Cara ou Coroa',
+            description=f'A moeda escolhida foi: {random.choice(["CARA", "COROA"])}\n'
+                        f'e você escolheu: {caracoroa.upper()}',
+            colour=discord.Colour.blue()
 
-    )
+        )
 
-    embed.set_thumbnail(url='https://gartic.com.br/imgs/mural/ja/jaqueroque/cara-ou-coroa.png')
-    await ctx.send(embed = embed)
-    
+        embed.set_thumbnail(url='https://gartic.com.br/imgs/mural/ja/jaqueroque/cara-ou-coroa.png')
+        await ctx.send(embed = embed)
+    else:
+        await ctx.send('Você deve digitar cara ou coroa após o comando!')
+        l()
+        print('Usuário digitou incorretamente.')
+        print(f'Usuário digitou "{caracoroa}" e devia ser ou cara ou coroa!')
+        l()
+
 client.run\
     ('TOKEN')
